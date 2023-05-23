43e384af6d76c435e8a91c3b86c3fc0644caeaa9
TOMÁS KALINSCKY <47494132@ort>
2023-05-23 10:37:04 -0300

    proyecto terminado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_bis.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Pais.cs
@@ -0,0 +1,16 @@
+public class Pais 
+{
+    public string Nombre {get; private set;}
+    public string imgBandera {get; private set;}
+    public int Poblacion {get; private set;}
+    public DateTime FechaIndependencia{get; private set;}
+    public string AtractivosTuristicos {get; private set;}
+
+    public Pais (string Nombre, string imgBandera, int Poblacion, DateTime FechaIndependencia, string AtractivosTuristicos){
+        this.Nombre=Nombre;
+        this.imgBandera=imgBandera;
+        this.Poblacion=Poblacion;
+        this.FechaIndependencia=FechaIndependencia;
+        this.AtractivosTuristicos=AtractivosTuristicos;
+    }
+}
